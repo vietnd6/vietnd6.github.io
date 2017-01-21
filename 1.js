dd9e8f148df66753b38009fcd5a537166777327d
vietnd6 <vietnd6@gmail.com>
2017-01-21 23:57:47 +0700

    xu ly facebook comment 2

--- 1.js
+++ 1.js
@@ -10,6 +10,12 @@
  	  	return false ; 
  	  
  	 })
+ 	
+	$(".cm").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#phanhoi').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
 
 })  
  
