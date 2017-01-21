de117b1a68d3977ad7001e850e8a8c99e8ee5d6b
vietnd6 <vietnd6@gmail.com>
2017-01-21 23:46:03 +0700

    html cho khoi kinh nghiem 2

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
 ﻿ $(function(){
- 	 
+ 	 	 
+ 	 $(".xuong,.kn").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoiedu').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+ 	$(".sp").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoisanpham').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+
 })  
  
